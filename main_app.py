0e95890bfd9e822b12126d3987960a0ab14fa7a2
shobhitsingh <shobhitsingh232@gmail.com>
2022-11-20 17:53:30 +0530

    first commit

---
+++ main_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+@app.route('/webhook')
+def webhook():
+    return 'webhook App with Python Flask!'
+
+app.run(host='0.0.0.0', port=81)
